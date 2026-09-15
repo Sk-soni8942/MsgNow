983f4ec5b89c24cafc946bc214e9379c2ac06ec5
SHYAM KUMAR SONI <shyamkumarsoni8942@gmail.com>
2026-09-15 16:25:03 +0530

    Update userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -52,7 +52,7 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-//api/user?search=Manoj
+
 
 const allUsers = asyncHandler(async (req, res) => {
   const keyword = req.query.search
